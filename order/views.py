5544d5f87d9fd8e7f9cb3a377f334c645928a0fa
chengsy <chengsy@cmgos.com>
2022-08-11 14:14:50 +0800

    ++

--- order/views.py
+++ order/views.py
@@ -3,7 +3,7 @@ from .models import Order, OrderStatus
 order_api = Router()
 
 
-@order_api.get("/undetermined_order", auth=None, response=None)
+@order_api.get("/undetermined_order/", auth=None, response=None)
 def undetermined_order(request, order_num: str):
     order = Order.objects.filter(order_num=order_num)
     if order.exists():
